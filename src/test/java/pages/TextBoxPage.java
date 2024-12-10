af482b5fc0a272819c3928b83ed2cead29578447
Toalra <K0$k@$1mk@2950>
2024-12-10 23:00:09 +0300

    Check spec color

--- src/test/java/pages/TextBoxPage.java
+++ src/test/java/pages/TextBoxPage.java
@@ -11,6 +11,9 @@ public class TextBoxPage {
     String itemName;
     String itemBasketName;
     String sizeBasketItem1;
+    String itemSpecColor;
+    String itemSpecSize;
+    String itemSpecBrend;
     String item2BasketName;
     String item2Name;
     String sizeBasketItem2;
@@ -20,74 +23,95 @@ public class TextBoxPage {
     String itemsCount;
     String itemsBasketCount;
     String price1;
-    String price2;
     String price3;
     String priceBasketItem1;
     String priceBasketItem2;
     String priceBasketItem3;
     SelenideElement
-        catalogy = $(".container mega-burger__inner"),
-        selectCatalogy = $("#rubrics-toggle"),
+            scrollElement = $(".card-product-layout__area-content"),
+            catalogy = $(".container mega-burger__inner"),
+            selectCatalogy = $("#rubrics-toggle"),
     //1 товар
-        selectCategory1 = $("[data-idx='0']"), //Мужчины
-        selectItem1 = $("div .rubrics-items-grid").$(".product-listing-card", 3), //выбор товара
-        selectSizeItem1 = $("[data-selector='options-group:root']")
-                .$(".options-group__list").$(".options-group__list-item", 0), //размер
-        addToBasket = $("[title='Добавить в корзину']"),
+            selectCategory1 = $("[data-idx='0']"), //Мужчины
+            selectItem1 = $("div .rubrics-items-grid").$(".product-listing-card", 3), //выбор товара
+            selectSizeItem1 = $("[data-selector='options-group:root']")
+                    .$(".options-group__list").$(".options-group__list-item", 0), //размер
+            addToBasket = $("[title='Добавить в корзину']"),
 
     //2 товар
-        selectCategory2 = $("[data-idx='1']"), //Женщины
-        selectItem2 = $("div .rubrics-items-grid").$(".product-listing-card", 2), //выбор товара
-        selectSizeItem2 = $("[data-selector='options-group:root']")
-                .$(".options-group__list").$(".options-group__list-item", 1), //размер,
+            selectCategory2 = $("[data-idx='1']"), //Женщины
+            selectItem2 = $("div .rubrics-items-grid").$(".product-listing-card", 2), //выбор товара
+            selectSizeItem2 = $("[data-selector='options-group:root']")
+                    .$(".options-group__list").$(".options-group__list-item", 1), //размер,
 
     //3 товар
-        selectCategory3 = $("[data-idx='4']"), //Аксессуары
-        selectItem3 = $("div .rubrics-items-grid").$(".product-listing-card", 2), //выбор товара
-        openBasket = $("[data-selector='basket-desktop']"),
+            selectCategory3 = $("[data-idx='4']"), //Аксессуары
+            selectItem3 = $("div .rubrics-items-grid").$(".product-listing-card", 2), //выбор товара
+            openBasket = $("[data-selector='basket-desktop']"),
 
     //ПРОВЕРКИ
     //Количество
-        checkCountItems = $(".toolbar-item__bage"),
-        checkCountItemsBasket = $(".basket-summary__btn-more").$(".js-more"),
+            checkCountItems = $(".toolbar-item__bage"),
+            checkCountItemsBasket = $(".basket-summary__btn-more").$(".js-more"),
     //Размеры
-        checkInBasketSizeItem1 = $(".group-items__content") //размер 1-го товара в корзине
+            checkInBasketSizeItem1 = $(".group-items__content") //размер 1-го товара в корзине
                     .$(".cart-item-default", 0)
                     .$(".cart-item-default__area-content")
                     .$(".cart-item-default__props")
                     .$(".cart-item-default__props-item", 0),
-        checkInBasketSizeItem2 = $(".group-items__content") //размер 2-го товара в корзине
-                .$(".cart-item-default", 1)
-                .$(".cart-item-default__area-content")
-                .$(".cart-item-default__props")
-                .$(".cart-item-default__props-item", 0),
+            checkInBasketSizeItem2 = $(".group-items__content") //размер 2-го товара в корзине
+                    .$(".cart-item-default", 1)
+                    .$(".cart-item-default__area-content")
+                    .$(".cart-item-default__props")
+                    .$(".cart-item-default__props-item", 0),
 
     //Цвет
-        checkColorItem3 = $(".group-items__content") //цвет 3-го товара в корзине
-            .$(".cart-item-default").$(".cart-item-default__area-content")
-            .$(".cart-item-default__props"),
+            checkColorItem3 = $(".group-items__content") //цвет 3-го товара в корзине
+                    .$(".cart-item-default").$(".cart-item-default__area-content")
+                    .$(".cart-item-default__props"),
 
 
     //Названия товаров
-        findTitleOfItem = $("#name"), //название на товаре
-        checkTitleOfItem1 = $(".js-items").$(".cart-item-default", 0)
-             .$(".cart-item-default__title"), //название 1-го в корзине
-        checkTitleOfItem2 = $(".js-items").$(".cart-item-default", 0)
-             .$(".cart-item-default__title"), //название 2-го в корзине
-        checkTitleOfItem3 = $(".js-groups").$(".group-items", 0)
-              .$(".group-items__content").$(".cart-item-default")
-              .$(".cart-item-default__area-content").$(".cart-item-default__title"),
+            findTitleOfItem = $("#name"), //название на товаре
+            checkTitleOfItem1 = $(".js-items").$(".cart-item-default", 0)
+                     .$(".cart-item-default__title"), //название 1-го в корзине
+            checkTitleOfItem2 = $(".js-items").$(".cart-item-default", 0)
+                     .$(".cart-item-default__title"), //название 2-го в корзине
+            checkTitleOfItem3 = $(".js-groups").$(".group-items", 0)
+                     .$(".group-items__content").$(".cart-item-default")
+                     .$(".cart-item-default__area-content").$(".cart-item-default__title"),
     //Цена
-        checkPrice = $(".price__regular"),
-        checkBasketPriceItem1 = $(".basket__block-groups").$(".group-items", 1)
-                .$(".group-items__content").$(".cart-item-default", 0)
-                .$(".cart-item-default__area-price").$(".cart-item-default__price"),
-        checkBasketPriceItem2 = $(".basket__block-groups").$(".group-items", 1)
-                .$(".group-items__content").$(".cart-item-default", 1)
-                .$(".cart-item-default__area-price").$(".cart-item-default__price"),
-        checkBasketPriceItem3 = $(".basket__block-groups").$(".group-items", 0)
-                .$(".group-items__content").$(".cart-item-default")
-                .$(".cart-item-default__area-price");
+            checkPrice = $(".price__regular"),
+            checkBasketPriceItem1 = $(".basket__block-groups").$(".group-items", 1)
+                    .$(".group-items__content").$(".cart-item-default", 0)
+                    .$(".cart-item-default__area-price").$(".cart-item-default__price"),
+            checkBasketPriceItem2 = $(".basket__block-groups").$(".group-items", 1)
+                    .$(".group-items__content").$(".cart-item-default", 1)
+                    .$(".cart-item-default__area-price").$(".cart-item-default__price"),
+            checkBasketPriceItem3 = $(".basket__block-groups").$(".group-items", 0)
+                    .$(".group-items__content").$(".cart-item-default")
+                    .$(".cart-item-default__area-price"),
+
+    //Характеристики
+
+            specsItemColor = $(".card-product-layout__content-col-tabs")
+                    .$(".card-product-layout__block-parameters")
+                    .$(".parameters-list-group", 2)
+                    .$(".parameters-list")
+                    .$("parameters-list__item")
+                    .$(".parameters-list__detail"),
+            specsItemSize = $(".card-product-layout__content-col-tabs")
+                    .$(".card-product-layout__block-parameters")
+                    .$(".parameters-list-group", 1)
+                    .$(".parameters-list")
+                    .$("parameters-list__item")
+                    .$(".parameters-list__detail"),
+            specsItemBrend = $(".card-product-layout__content-col-tabs")
+                    .$(".card-product-layout__block-parameters")
+                    .$(".parameters-list-group", 0)
+                    .$(".parameters-list")
+                    .$(".parameters-list__item", 2)
+                    .$(".parameters-list__detail");
 
 
     public TextBoxPage openWebSite() {
@@ -194,6 +218,18 @@ public class TextBoxPage {
     }
     public String getPriceItem1() {
         return price1;
+    }
+    @DisplayName("Парсинг характеристик")
+    public TextBoxPage parsSpecsColor() {
+        itemSpecColor = specsItemColor.getText();
+        return this;
+    }
+    public String getSpecColor() {
+        return itemSpecColor;
+    }
+    public TextBoxPage scrollSpec() {
+        scrollElement.scrollTo();
+        return this;
     }
 
 //------------------------------------------------------------------------------------
--- src/test/java/tests/SpecificationsItemsPageOpject.java
+++ src/test/java/tests/SpecificationsItemsPageOpject.java
@@ -1,4 +1,47 @@
 package tests;
 
-public class SpecificationsItemsPageOpject {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pages.TextBoxPage;
+
+import static com.codeborne.selenide.Selenide.refresh;
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SpecificationsItemsPageOpject extends TestBase {
+    TextBoxPage openWebPage = new TextBoxPage();
+    TextBoxPage specsItems = new TextBoxPage();
+
+    @DisplayName("1. Открытие Web-сайта")
+    @Test
+    void test01() {
+        step("Открытие сайта", () -> {
+            openWebPage
+                    .openWebSite();
+            refresh();
+        });
+    }
+
+    @DisplayName("1. Открытие карточки 1-го товара")
+    @Test
+    void test02() {
+        step("Отобржение карточки 1-го товара", () -> {
+            specsItems
+                    .getCatalogy()
+                    .getCategory1()
+                    .getItem1()
+                    .nameItem1()
+                    .scrollSpec()
+                    .parsSpecsColor();
+        });
+        step("Проверка характеристики цвета 1-го товара", () -> {
+
+            String itemColor = specsItems.getItemName();
+            String itemSpecColor = specsItems.getSpecColor();
+            assertTrue(itemColor.contains(itemSpecColor));
+
+            System.out.println(specsItems.getItemName());
+            System.out.println(specsItems.getSpecColor());
+        });
+    }
 }
